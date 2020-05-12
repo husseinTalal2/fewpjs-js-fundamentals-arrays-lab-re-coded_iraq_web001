5ce3446bf8871591d235f400b9feaf9aa09e983c
Hussein Talal <hossientalal2@gmail.com>
2020-05-12 11:54:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const prepend =['Milo', 'Otis', 'Garfield'];
 const removeLast = ['Milo', 'Otis', 'Garfield'];
 const append =['Milo', 'Otis', 'Garfield'];
 const removeFirst = ['Milo', 'Otis', 'Garfield'];
+prepend.unshift('Odie');
+removeLast.pop();
